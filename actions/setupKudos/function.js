a4d5c998e862ed43092f0d4c21237eb9e785b8b6
Luke Andrews <luke@attaboy.ca>
2018-08-13 15:01:34 +0000

    Added check for missing env vars in the setup action

--- actions/setupKudos/function.js
+++ actions/setupKudos/function.js
@@ -1,3 +1,11 @@
 function(channel, ellipsis) {
-  
+  const unsetVars = ["KUDOS_SHEET_NAME", "KUDOS_SHEET_ID"].filter((ea) => !ellipsis.env[ea]);
+let result = "";
+if (unsetVars.length === 2) {
+  result = `The following environment variables need to be set to save kudos to a spreadsheet:
+${unsetVars.join(", ")}`;
+} else if (unsetVars.length === 1) {
+  result = `The environment variable ${unsetVars[0]} needs to be set to save kudos to a spreadsheet.`
+}
+ellipsis.success(result);
 }
--- actions/setupKudos/response.md
+++ actions/setupKudos/response.md
@@ -1,1 +1,3 @@
 When people give kudos, I'll post them in the channel {channel}.
+
+{successResult}
